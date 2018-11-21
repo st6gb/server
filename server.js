ddca1627a4bdbb15e2dc9581f256cc6fed5bc6ce
Pavel_Buldakov <pavel_buldakov@epam.com>
2018-11-21 14:04:15 +0400

    start server on express

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+var bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000
+
+app.all('*', function(req, res, next) {
+  var origin = req.get('origin'); 
+  res.header('Access-Control-Allow-Origin', origin);
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.get('/', (req, res) => {
+  res.json({'lala':'Hello World!'})
+  }
+)
+
+app.get('/file/:name', (req, res) => {
+  const options = {
+    root: __dirname + '/',
+    dotfiles: 'deny',
+    headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+    }
+  };
+  const filename = req.params.name;
+  res.status(200).sendFile(filename, options)
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
